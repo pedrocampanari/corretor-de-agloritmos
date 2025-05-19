2ab45fbf3ebd4e75d1eac23f85b8bb63dfbdd93e
pedrocampanari <pedrocampanari09@hotmail.com>
2025-05-19 15:18:53 -0400

    v1.3

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ class App {
     enable_events() {
         document.getElementsByClassName("task-button")[0].addEventListener("mouseover", ()=>{
             let btn = document.getElementsByClassName("icon")[0];
-            btn.src = 'assets/icons/dark/send.png'
+            btn.src = '../assets/icons/dark/send.png'
 
         });
 
         document.getElementsByClassName("task-button")[0].addEventListener("mouseout", ()=>{
             let btn = document.getElementsByClassName("icon")[0];
-            btn.src = 'assets/icons/light/send.png'
+            btn.src = '../assets/icons/light/send.png'
         });
     }
 
